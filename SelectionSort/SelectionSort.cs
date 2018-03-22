c51ce7c818280c37139382ea311ee298f4c23f58
trungngotdt <trungngotdt@outlook.com.vn>
2018-03-23 00:05:16 +0700

    Selection Sort Algorithm

---
+++ SelectionSort/SelectionSort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SelectionSort
+{
+    public class SelectionSort
+    {
+        private int[] array;
+        public SelectionSort(int[] array)
+        {
+            this.Array = array;
+        }
+
+        public int[] Array { get => array; set => array = value; }
+
+        public void Selection()
+        {
+            if (Array==null)
+            {
+                return;
+            }
+            for (int i = Array.Length-1; i >=1; i--)
+            {
+                int index = i;
+                for (int j = 0; j < i; j++)
+                {
+                    if (Array[j]>Array[index])
+                    {
+                        index = j;
+                    }
+                    int temp = Array[index];
+                    Array[index] = Array[i];
+                    Array[i] = temp;
+                }
+            }
+        }
+    }
+}
---
+++ TestSelectionSort/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SelectionSort.SelectionSort sort=new SelectionSort.SelectionSort(new int[]{29,10,14,37,13 });
+            sort.Selection();
+
+        }
+    }
+}
