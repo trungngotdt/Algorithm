0efbe9afba789557fc94abc9c32359e7b8e30270
trungngotdt <trungngotdt@outlook.com.vn>
2018-03-27 20:52:38 +0700

    Bubble Sort

---
+++ BubbleSort/BubbleSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BubbleSortLib
+{
+    public class BubbleSort
+    {
+        private int[] array;
+
+        public int[] Array { get => array; set => array = value; }
+        public BubbleSort(int[] array)
+        {
+            this.Array = array;
+        }
+
+        public void Sort()
+        {
+            int n = Array.Length;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n-i-1; j++)
+                {
+                    if (Array[j]>Array[j+1])
+                    {
+                        int temp = Array[j];
+                        Array[j] = Array[j + 1];
+                        Array[j + 1] = temp;
+                    }
+                }
+                
+            }
+        }
+    }
+}
---
+++ BubbleSortTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using BubbleSortLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSortTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BubbleSort bubbleSort = new BubbleSort(new int[] {9,5,1,7,4,6,8,2,3 });
+            bubbleSort.Sort();
+        }
+    }
+}
